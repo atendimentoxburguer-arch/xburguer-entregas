cdea08b87c0004a9af34752611701c11090f0760
atendimentoxburguer-arch <atendimentoxburguer@gmail.com>
2026-09-18 16:31:12 -0300

    Fecha versão da revisão arquitetural

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = '20260918-arch1';
+  const version = '20260918-arch2';
 
   // O HTML pode permanecer aberto por dias em outro computador. Atualizamos
   // a folha principal pelo carregador para forçar a mesma versão visual em todos.
