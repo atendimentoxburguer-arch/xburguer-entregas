e46c9ba1773e51893ebe278e25bef44359be17d1
atendimentoxburguer-arch <atendimentoxburguer@gmail.com>
2026-09-18 16:39:43 -0300

    Alinha app.js à versão 20260918-arch3

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = '20260918-arch2';
+  const version = '20260918-arch3';
 
   // O HTML pode permanecer aberto por dias em outro computador. Atualizamos
   // a folha principal pelo carregador para forçar a mesma versão visual em todos.
