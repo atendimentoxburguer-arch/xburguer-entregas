9bab74aa5510360872e6e727f1732ef4b2af65c9
atendimentoxburguer-arch <atendimentoxburguer@gmail.com>
2026-09-21 00:09:36 -0300

    Atualiza app.js para proteção de dias abertos

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = '20260918-arch3';
+  const version = '20260921-open-day-v1';
 
   // O HTML pode permanecer aberto por dias em outro computador. Atualizamos
   // a folha principal pelo carregador para forçar a mesma versão visual em todos.
